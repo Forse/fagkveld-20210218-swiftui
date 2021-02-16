e6a6242051cda11dff2d0af60ac662aeeb7a9f81
Joakim Stien <joakim.stien@icloud.com>
2021-02-16 18:36:28 +0100

    Initial Commit

---
+++ ForseDemo/ForseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForseDemoApp.swift
+//  ForseDemo
+//
+//  Created by Joakim Stien on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ForseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
