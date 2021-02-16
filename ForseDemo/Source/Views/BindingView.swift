772a0fa5bfdd65545149bd426ad2ee9e1641cb0b
Joakim Stien <joakim.stien@icloud.com>
2021-02-16 21:07:36 +0100

    Added BindingView

---
+++ ForseDemo/Source/Views/BindingView.swift
@@ -0,0 +1,26 @@
+import Foundation
+import SwiftUI
+
+struct BindingView: View {
+    @State private var isOn: Bool = false
+
+    var body: some View {
+        VStack {
+            Toggle("Is this thing on?", isOn: $isOn)
+
+            if isOn {
+                Text("Yup, it's on!")
+            } else {
+                Text("Nope! :(")
+            }
+        }
+    }
+}
+
+struct BindingView_previews: PreviewProvider {
+    static var previews: some View {
+        BindingView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
